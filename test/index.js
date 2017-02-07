b02d995e1dc98b1341594a84af4d0fed474e8622
Andrew Shaffer <andrew@weo.io>
2017-02-06 18:44:11 -0800

    fix string seeding

--- test/index.js
+++ test/index.js
@@ -13,5 +13,7 @@ test('should work', function (t) {
   var r = srand(1)
   t.equal(r(), 0.2511917009602195)
   t.equal(r(), 0.5453317901234568)
+
+  t.equal(srand('test')(), 0.5011574074074074)
   t.end()
 })
